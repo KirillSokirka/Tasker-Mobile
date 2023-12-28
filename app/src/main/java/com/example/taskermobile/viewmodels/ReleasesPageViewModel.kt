7041e84b2d2637fc4ab23bb945a0484d9de68453
dimaskvitka <dimaskvitka@gmail.com>
2023-12-28 14:27:06 +0200

    AddRelease functionality added WIP

--- app/src/main/java/com/example/taskermobile/viewmodels/ReleasesPageViewModel.kt
+++ app/src/main/java/com/example/taskermobile/viewmodels/ReleasesPageViewModel.kt
@@ -3,6 +3,7 @@ package com.example.taskermobile.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.example.taskermobile.model.release.ReleaseModel
 import com.example.taskermobile.model.release.ReleasePreviewModel
 import com.example.taskermobile.service.ReleaseApiService
 import com.example.taskermobile.utils.ApiResponse
@@ -17,10 +18,20 @@ class ReleasesPageViewModel(
     private val _releasesResponse = MutableLiveData<ApiResponse<List<ReleasePreviewModel>>>()
     val releasesResponse: LiveData<ApiResponse<List<ReleasePreviewModel>>> = _releasesResponse
 
+    private val _releaseResponse = MutableLiveData<ApiResponse<ReleaseModel>>()
+    val releaseResponse: LiveData<ApiResponse<ReleaseModel>> = _releaseResponse
+
     fun getAll(projectId: String) {
         viewModelScope.launch {
             apiRequestFlow { releaseApiService.getByProject(projectId) }
                 .collect { apiResponse -> _releasesResponse.value = apiResponse }
+        }
+    }
+
+    fun create(model: ReleaseModel) {
+        viewModelScope.launch {
+            apiRequestFlow { releaseApiService.create(model) }
+                .collect { apiResponse -> _releaseResponse.value = apiResponse }
         }
     }
 }
